9b4d29a7404a5942dcda1b0298616da3c09ed5a4
mproffa <111048766+mproffa@users.noreply.github.com>
2023-05-15 21:53:47 +0300

    valmis

---
+++ js/Javascriptform.js
@@ -0,0 +1,24 @@
+//lisätään napin kuuntelijafunktio
+document.querySelector("#note").addEventListener("click", addNote);
+
+let body = document.querySelector(".lomake");
+let checkbox = document.getElementById("important");
+
+//luodaan funktio addNote
+function addNote() {
+    let h3 = document.createElement("h3");
+    let p = document.createElement("p");
+    let h5 = document.createElement("h5");
+    p.textContent = viesti.value;
+    h5.textContent = nimi.value;
+    if (checkbox.checked) {
+
+        h3.classList.add("important");
+        p.classList.add("important");
+        h5.classList.add("important");
+
+    }
+
+    //kaikki elementit bodyn sisään
+    body.append(h3, h5, p, document.createElement("hr"));
+}
